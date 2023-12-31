bfbe1767f01faeab3ac7e3dcb7d44d040d971bfe
Mubashar Murtza <Mubashar222@gmail.com>
2023-12-31 18:12:37 +0500

    Misc changes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,78 +1,77 @@
 import React from 'react';
 import Link from 'next/link';
-import { FiMail } from "react-icons/fi";
-import { RxPerson } from "react-icons/rx";
+import { FiMail } from 'react-icons/fi';
+import { RxPerson } from 'react-icons/rx';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { BsPersonAdd } from 'react-icons/bs';
-import { GiSpeedometer } from "react-icons/gi"
-import { IoSettingsOutline } from "react-icons/io5";
-import { AiOutlineHome } from "react-icons/ai";
-import './main.css'
-import { FaRegAddressCard } from "react-icons/fa";
+import { GiSpeedometer } from 'react-icons/gi';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { AiOutlineHome } from 'react-icons/ai';
+import './main.css';
+import { FaRegAddressCard } from 'react-icons/fa';
 
 const Sidebar = ({ children }) => {
   return (
-    <div className='flex'>
-      <div className=' sidecolor fixed w-20 text-2xl h-screen p-4 border-r-[1px] flex flex-col justify-between'>
-        <div className='flex flex-col items-center'>
-          <Link href='/'>
-            <div className='text-gray-700 font-black p-3 rounded-lg inline-block'>
+    <div className="flex">
+      <div className="sidecolor fixed w-20 text-2xl h-screen p-4 border-r-[1px] flex flex-col justify-between overflow-y-auto">
+        <div className="flex flex-col items-center">
+          <Link href="/">
+            <div className="text-gray-700 font-black p-3 rounded-lg inline-block">
               <h1>Frize.</h1>
             </div>
           </Link>
-          <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
+          <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
 
-          <Link href='/'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <AiOutlineHome size={20} />
             </div>
           </Link>
 
-          <Link href='/market'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/market">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <GiSpeedometer size={20} />
             </div>
           </Link>
 
-          <Link href='/mail'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/mail">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <FiMail size={20} />
             </div>
           </Link>
 
-          <Link href='/customers'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/customers">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <RxPerson size={20} />
             </div>
           </Link>
 
-
-          <Link href='/orders'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/orders">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <HiOutlineShoppingBag size={20} />
             </div>
           </Link>
 
-          <Link href='/newper'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/newper">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <BsPersonAdd size={25} />
             </div>
           </Link>
 
-          <Link href='/todos'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/todos">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <FaRegAddressCard size={25} />
             </div>
           </Link>
 
-          <Link href='/settings'>
-            <div className='sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+          <Link href="/settings">
+            <div className="sidecolor hover:bg-blue-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
               <IoSettingsOutline size={25} />
             </div>
           </Link>
         </div>
       </div>
-      <main className='ml-20 w-full'>{children}</main>
+      <main className="ml-20 w-full">{children}</main>
     </div>
   );
 };
