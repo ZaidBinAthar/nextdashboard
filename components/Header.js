ff54300a24689fe51d4af7afbafbabd99bc70785
Zaid Athar <zaid.athar.2009@gmail.com>
2024-01-09 14:57:48 +0500

    Header resposive

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,6 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Header = () => {
 
-
   const showToast = () => {
     toast.success('Thanks To Like!', {
       position: 'top-center',
@@ -34,32 +33,32 @@ const Header = () => {
   };
 
   return (
-    <div className='flex justify-between px-4 pt-4'>
-      <div>
+    <div className='flex flex-col lg:flex-row justify-between px-4 pt-4'>
+      <div className='mb-4 lg:mb-0'>
         <h2 className='font-black text-gray-700 text-2xl'>Crypto Dashboard</h2>
       </div>
-      <div className='flex'>
-        <div className='pt-2'>
+      <div className='flex items-center'>
+        <div className='pt-2 mr-2'>
           <FaSearch size={20} />
         </div>
-        <input className='pl-3' type='text' placeholder='Search here...' />
-        <BsChatText onClick={showToast1} size={20} />
-        <div className='pl-8'>
-          <BsBell onClick={showToast} size={20} />
+        <input className='pl-3 py-1 rounded-md focus:outline-none' type='text' placeholder='Search here...' />
+        <BsChatText onClick={showToast1} size={20} className='ml-2 cursor-pointer' />
+        <div className='ml-2'>
+          <BsBell onClick={showToast} size={20} className='cursor-pointer' />
           <ToastContainer />
         </div>
-        <div className='pl-8'>
+        <div className='ml-2'>
           <Link href='/settings'>
-            <IoSettingsOutline size={25} />
+            <IoSettingsOutline size={25} className='cursor-pointer' />
           </Link>
         </div>
-        <div className='flex pl-8 pr-2'>
-          <p className='text-purple-800 font-black italic'>Zaid</p>
+        <div className='flex items-center ml-2'>
+          <p className='text-purple-800 font-black italic mr-1'>Zaid</p>
+          <CgProfile size={20} />
         </div>
-        <p className='pt-0.5'><CgProfile size={20} /></p>
       </div>
     </div>
   )
 }
 
-export default Header
+export default Header;
